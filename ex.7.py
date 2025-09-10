8278f6c6cc5333b2ed460797aafac689d536e774
Rashika-3 <rshiga396@gmail.com>
2025-09-10 13:53:15 +0530

    Add files via upload

--- ex.7.py
+++ ex.7.py
@@ -4,63 +4,41 @@ class Node:
         self.left = None
         self.right = None
 
+def build_tree():
+    data = input("Enter node value (or 'None' for no node): ")
+    if data.lower() == 'none':
+        return None
 
-# Insert function (BST style insertion)
-def insert(root, data):
-    if root is None:
-        return Node(data)
-    else:
-        if data < root.data:
-            root.left = insert(root.left, data)
-        else:
-            root.right = insert(root.right, data)
-    return root
+    node = Node(data)
+    print(f"Enter left child of {data}")
+    node.left = build_tree()
+    print(f"Enter right child of {data}")
+    node.right = build_tree()
+    return node
 
+def preorder(node):
+    if node:
+        print(node.data, end=' ')
+        preorder(node.left)
+        preorder(node.right)
 
-# Traversal functions
-def inorder(root):
-    if root:
-        inorder(root.left)
-        print(root.data, end=" ")
-        inorder(root.right)
+def inorder(node):
+    if node:
+        inorder(node.left)
+        print(node.data, end=' ')
+        inorder(node.right)
 
+def postorder(node):
+    if node:
+        postorder(node.left)
+        postorder(node.right)
+        print(node.data, end=' ')
 
-def preorder(root):
-    if root:
-        print(root.data, end=" ")
-        preorder(root.left)
-        preorder(root.right)
-
-
-def postorder(root):
-    if root:
-        postorder(root.left)
-        postorder(root.right)
-        print(root.data, end=" ")
-
-
-# Print tree sideways
-def print_tree(root, level=0, prefix="Root: "):
-    if root is not None:
-        print_tree(root.right, level + 1, "R----")
-        print("     " * level + prefix + str(root.data))
-        print_tree(root.left, level + 1, "L----")
-
-
-# Driver code
-if __name__ == "__main__":
-    root = None
-    elements = ['D', 'B', 'A', 'C', 'F', 'E']  # Slightly rearranged for BST clarity
-
-    for elem in elements:
-        root = insert(root, elem)
-
-    print("\nTree Structure:")
-    print_tree(root)
-
-    print("\nInorder Traversal:")
-    inorder(root)
-    print("\nPreorder Traversal:")
-    preorder(root)
-    print("\nPostorder Traversal:")
-    postorder(root)
+print("Build the binary tree:")
+root = build_tree()
+print("\nPreorder Traversal:")
+preorder(root)
+print("\nInorder Traversal:")
+inorder(root)
+print("\nPostorder Traversal:")
+postorder(root)
